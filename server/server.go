72e7244e9d7b0a09cee27b958b083c824dd13cb5
bohdanpodlisetskyi <bohdan@stryber.com>
2021-07-16 15:44:57 +0300

    server

---
+++ server/server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/bohdanstryber/grpc-helloworld"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
